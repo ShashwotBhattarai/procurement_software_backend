c2ca656bcac8a4f77f78cfab49fa1bd7ccb04000
Shashwot Bhattarai <shashwot.tech@gmail.com>
2023-07-31 11:31:28 +0545

    removed  guards from get apis of item and site

--- src/admin/admin_controllers/items.controllers.ts
+++ src/admin/admin_controllers/items.controllers.ts
@@ -13,15 +13,16 @@ import { ItemsDto } from 'src/dtos/Items.dto';
 import { ItemsService } from '../admin_services/items.service';
 import { AdminAuthGuard} from 'src/auth/adminAuth.guard';
 
-@UseGuards(AdminAuthGuard)
+
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemService: ItemsService) { }
+  @UseGuards(AdminAuthGuard)
   @Post()
   create(@Body() itemsDto: ItemsDto) {
     return this.itemService.create(itemsDto);
   }
-
+  @UseGuards(AdminAuthGuard)
   @Put(':id')
   update(@Body() itemsDto: ItemsDto, @Param('id') id: string) {
     return this.itemService.updateItem(id, itemsDto);
@@ -32,11 +33,12 @@ export class ItemsController {
     return this.itemService.findAll();
   }
 
+  @UseGuards(AdminAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.itemService.findOne(id);
   }
-
+  @UseGuards(AdminAuthGuard)
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.itemService.delete(id);
--- src/admin/admin_controllers/site.controllers..ts
+++ src/admin/admin_controllers/site.controllers..ts
@@ -4,15 +4,16 @@ import { SiteService } from '../admin_services/site.service';
 import { SiteDto } from 'src/dtos/Site.dto';
 import { AdminAuthGuard } from 'src/auth/adminAuth.guard';
 
-@UseGuards(AdminAuthGuard)
+
 @Controller('site')
 export class SiteController {
     constructor(private readonly siteService: SiteService) { }
+    @UseGuards(AdminAuthGuard)
     @Post()
     create(@Body() addSiteDto: SiteDto) {
         return this.siteService.create(addSiteDto);
     }
-
+    @UseGuards(AdminAuthGuard)
     @Put(":id")
     update(@Body() siteDto: SiteDto, @Param('id') id: string) {
         return this.siteService.updateItem(id, siteDto);
@@ -23,11 +24,12 @@ export class SiteController {
         return this.siteService.findAll();
     }
 
+    @UseGuards(AdminAuthGuard)
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.siteService.findOne(id);
     }
-
+    @UseGuards(AdminAuthGuard)
     @Delete(':id')
     delete(@Param('id') id: number) {
         return this.siteService.delete(id);
